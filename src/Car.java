2f52095760fe10c0a81b16d35e057d5cbb9a6fbb
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2023-01-10 18:01:06 +0300

    задание 2 выполнено

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,15 @@ public class Car {
     int year;
     String contry;
 
+    public Car(String brand, String model, double engineVolume, String color, int year, String contry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.contry = contry;
+    }
+
     @Override
     public String toString() {
         return brand+" "+model+", "+year+" год выпуска, "+color+" цвет кузова, сборка в "+contry+", объем двигателя "+engineVolume+" л.";
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,47 @@
+import java.security.AllPermission;
+
 public class Main {
     public static void main(String[] args) {
-        Car granta = new Car();
-        granta.brand = "Lada";
+        Car granta = new Car("Lada","Granta",1.7,"желтый",2015,"России");
+     /*   granta.brand = "Lada";
         granta.model = "Granta";
         granta.year=2015;
         granta.contry = "России";
         granta.color = "желтый";
-        granta.engineVolume = 1.7;
-        Car a8=new Car();
-        a8.brand = "Audi";
-        a8.model = "a8 50 L TDI quattro";
+        granta.engineVolume = 1.7;*/
+        Car a8=new Car("Audi","A8 50 L TDI quattro",3.0,"черный",2020,"Германии");
+       /* a8.brand = "Audi";
+        a8.model = "A8 50 L TDI quattro";
         a8.year = 2020;
         a8.contry = "Германии";
         a8.color = "черный";
-        a8.engineVolume = 3.0;
-        Car z8 = new Car();
-        z8.brand = "BMW";
-        z8.model = "z8";
+        a8.engineVolume = 3.0;*/
+        Car z8 = new Car("BMW","Z8",3.0,"черный",2021,"Германии");
+       /* z8.brand = "BMW";
+        z8.model = "Z8";
         z8.contry = "Германии";
         z8.year=2021;
         z8.color = "черный";
-        z8.engineVolume = 3.0;
-        Car sportage = new Car();
-        sportage.model = "Sportage 4-го поколения";
+        z8.engineVolume = 3.0;*/
+        Car sportage = new Car("KIA","Sportage 4-го поколения",2.4,"красный",2018,"Южной Корее");
+        /*sportage.model = "Sportage 4-го поколения";
         sportage.brand = "KIA";
         sportage.year=2018;
         sportage.contry = "Южной Корее";
         sportage.color = "красный";
-        sportage.engineVolume = 2.4;
-        Car avante = new Car();
-        avante.brand = "Hyundai";
+        sportage.engineVolume = 2.4;*/
+        Car avante = new Car("Hyundai","Avante",1.6,"оранжевый",2016,"Южной Корее");
+        /*avante.brand = "Hyundai";
         avante.model = "Avante";
         avante.contry = "Южной Корее";
         avante.color = "оранжевый";
         avante.engineVolume = 1.6;
-        avante.year=2016;
+        avante.year=2016;*/
         System.out.println(granta);
         System.out.println(a8);
         System.out.println(z8);
         System.out.println(sportage);
         System.out.println(avante);
+
     }
 }
